c2c37717f1b07ec72a117569c4df1bb22dc3d3fd
khalid225 <msart4real@yahoo.com>
2025-07-12 03:05:41 +0100

    Initial Commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from 'axios';
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bestShows: []
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount success")
+    axios.get('/api/data')
+      .then(res => {
+        console.log("data recieved: ", res.data);
+        this.setState({ bestShows: res.data[0] });
+      })
+      .catch(alert);
+  }
+
+
+  render() {
+    console.log("render bestShows: ", this.state.bestShows)
+    return (
+      <div>
+        azure-mern-demo
+        <ul>
+          {
+            Object.keys(this.state.bestShows).map((cur, idx) => (
+              <li>{cur} - {this.state.bestShows[cur]} </li>
+            ))
+          }
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
